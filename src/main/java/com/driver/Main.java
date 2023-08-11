bb7409acd74879ed47ea9c513e702eeee6da9f7a
Siddharth <siddharth05b@gmail.com>
2023-08-11 16:37:37 +0530

    Tests added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,19 +9,5 @@ public class Main {
     }
 }
 
-class Product {
 
-    public int product(int x, int y) {
-        return x * y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-
-    public double product(double x, double y) {
-        return x * y;
-    }
-
-}
 
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,27 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+
+import com.driver.Product;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+    @Test
+    public void testProductWithIntegers() {
+        Product p = new Product();
+
+        int result1 = p.product(2, 3);
+        assertEquals(6, result1);
+
+        int result2 = p.product(2, 3, 4);
+        assertEquals(24, result2);
+    }
+
+    @Test
+    public void testProductWithDoubles() {
+        Product p = new Product();
+
+        double result = p.product(2.0, 3.0);
+        assertEquals(6.0, result, 0.0001); // Using delta for double comparison
+    }
 }
